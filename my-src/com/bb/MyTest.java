58a204ea54a952f60a4f20f0f70817c86e6ac571
zhangsan77 <jiebai720@qq.com>
2021-11-12 18:22:07 +0800

    lab 1

--- my-src/com/bb/MyTest.java
+++ my-src/com/bb/MyTest.java
@@ -1,9 +1,17 @@
-package simpledb;
+package com.bb;
 
 public class My {
 
 
     public static void main(String args[]){
+
+
+
+        for (int i = 0; i < 17; i++) {
+
+            int index = i/8 ;
+            System.out.println( i+"---"+ index );
+        }
 
         int[] lengths = new int[] { 1, 2, 1000 };
 
